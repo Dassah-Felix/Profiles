247965b0d325c9c97a0d81700a0ed05595be0842
Dassah Felix <101268084+Dassah-Felix@users.noreply.github.com>
2023-01-25 05:53:39 -0800

    UPDATED
    numbers where changed

--- src/Writers.js
+++ src/Writers.js
@@ -1,31 +1,31 @@
 const Writers = [
   {
     id: 1,
-    name: "Jamila Lorde",
+    name: "keishi mon",
     avatar: "audre_lorde",
     email: "jamila23@gmail.com",
-    phone: "0554318132",
+    phone: "05868698754",
   },
   {
     id: 2,
     name: "Jamila Lorde",
     avatar: "belle_hooks",
     email: "jamila23@gmail.com",
-    phone: "0554318132",
+    phone: "78678588486",
   },
   {
     id: 3,
     name: "Jamila Lorde",
     avatar: "du_bois",
     email: "jamila23@gmail.com",
-    phone: "0554318132",
+    phone: "82865756247",
   },
   {
     id: 4,
     name: "Jamila Lorde",
     avatar: "james_baldwin",
     email: "jamila23@gmail.com",
-    phone: "0554318132",
+    phone: "8758768658094",
   },
   {
     id: 5,
